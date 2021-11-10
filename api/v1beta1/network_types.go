f06d78780fd713de60301cc37eee9f1a179249fa
janiskemper <janis.kemper@syself.com>
2021-11-10 13:18:34 +0100

    Refactoring network reconcilement and removing multiple subnets

--- api/v1beta1/network_types.go
+++ api/v1beta1/network_types.go
@@ -19,26 +19,24 @@ package v1beta1
 // NetworkSpec defines the desired state of the Hetzner Private Network.
 type NetworkSpec struct {
 	NetworkEnabled bool `json:"enabled"`
+
 	// Defines the cidrBlock of the Hetzner Network. A Subnet is required.
-	//+ optional
+	// +kubebuilder:default="10.0.0.0/16"
 	CIDRBlock string `json:"cidrBlock,omitempty"`
 
-	// Defines subnets. We don't have logic yet to support multiple subnets.
-	//+ optional
-	Subnets Subnets `json:"subnets,omitempty"`
+	// +kubebuilder:default="10.0.0.0/24"
+	SubnetCIDRBlock string `json:"subnetCidrBlock,omitempty"`
+
+	// +kubebuilder:default=eu-central
+	NetworkZone HCloudNetworkZone `json:"networkZone,omitempty"`
 }
 
 // NetworkStatus defines the observed state of the Hetzner Private Network.
 type NetworkStatus struct {
-	CIDRBlock      string            `json:"cidrBlock,omitempty"`
-	Subnets        Subnets           `json:"subnets,omitempty"`
 	ID             int               `json:"id,omitempty"`
 	Labels         map[string]string `json:"-"`
 	AttachedServer []int             `json:"attachedServer,omitempty"`
 }
-
-// Subnets is a slice of Subnet.
-type Subnets []SubnetSpec
 
 // HCloudRegion is a Hetzner Location
 // +kubebuilder:validation:Enum=fsn1;hel1;nbg1;ash
@@ -47,18 +45,12 @@ type HCloudRegion string
 // HCloudNetworkZone describes the Network zone.
 type HCloudNetworkZone string
 
-// SubnetSpec configures an Hetzner Cloud Subnet.
-type SubnetSpec struct {
-	CIDRBlock   string            `json:"cidrBlock,omitempty"`
-	NetworkZone HCloudNetworkZone `json:"networkZone,omitempty"`
-}
-
 // IsZero return if a private Network is set or not.
 func (s *NetworkSpec) IsZero() bool {
 	if len(s.CIDRBlock) > 0 {
 		return false
 	}
-	if len(s.Subnets) > 0 {
+	if len(s.SubnetCIDRBlock) > 0 {
 		return false
 	}
 	return true
