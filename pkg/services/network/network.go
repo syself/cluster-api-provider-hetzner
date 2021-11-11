330c6db2f5480b54908b8cda9a32c2894c576964
janiskemper <63146658+janiskemper@users.noreply.github.com>
2021-11-11 08:55:13 +0100

    Refactoring network reconcilement and removing multiple subnets (#12)

--- pkg/services/network/network.go
+++ pkg/services/network/network.go
@@ -3,6 +3,7 @@ package network
 
 import (
 	"context"
+	"fmt"
 	"net"
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
@@ -26,76 +27,30 @@ func NewService(scope *scope.ClusterScope) *Service {
 	}
 }
 
-func apiToStatus(network *hcloud.Network) *infrav1.NetworkStatus {
-	var subnets = make([]infrav1.SubnetSpec, len(network.Subnets))
-	for pos, n := range network.Subnets {
-		subnets[pos].NetworkZone = infrav1.HCloudNetworkZone(n.NetworkZone)
-		subnets[pos].CIDRBlock = n.IPRange.String()
-	}
-
-	attachedServerIDs := make([]int, 0, len(network.Servers))
-	for _, s := range network.Servers {
-		attachedServerIDs = append(attachedServerIDs, s.ID)
-	}
-
-	var status infrav1.NetworkStatus
-	status.ID = network.ID
-	status.CIDRBlock = network.IPRange.String()
-	status.Subnets = subnets
-	status.Labels = network.Labels
-	status.AttachedServer = attachedServerIDs
-	return &status
-}
-
-func (s *Service) labels() map[string]string {
-	clusterTagKey := infrav1.ClusterTagKey(s.scope.HetznerCluster.Name)
-	return map[string]string{
-		clusterTagKey: string(infrav1.ResourceLifecycleOwned),
-	}
-}
-
 // Reconcile implements life cycle of networks.
 func (s *Service) Reconcile(ctx context.Context) (err error) {
-	s.scope.Info("Reconciling network")
+	s.scope.Info("Reconciling network", "spec", s.scope.HetznerCluster.Spec.NetworkSpec)
 	if !s.scope.HetznerCluster.Spec.NetworkSpec.NetworkEnabled {
 		return nil
 	}
 
-	// update current status
-	networkStatus, err := s.actualStatus(ctx)
+	network, err := s.findNetwork(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to refresh networks")
+		return errors.Wrap(err, "failed to find network")
 	}
-	s.scope.HetznerCluster.Status.Network = networkStatus
-
-	if networkStatus != nil {
-		return nil
-	}
-
-	// set defaults if nothing is set
-	if s.scope.HetznerCluster.Spec.NetworkSpec.CIDRBlock == "" {
-		s.scope.HetznerCluster.Spec.NetworkSpec.CIDRBlock = "10.0.0.0/16"
-	}
-	if len(s.scope.HetznerCluster.Spec.NetworkSpec.Subnets) == 0 {
-		s.scope.HetznerCluster.Spec.NetworkSpec.Subnets = []infrav1.SubnetSpec{
-			{
-				NetworkZone: "eu-central",
-
-				CIDRBlock: "10.0.0.0/24",
-			},
+	if network == nil {
+		// create network
+		network, err = s.createNetwork(ctx, &s.scope.HetznerCluster.Spec.NetworkSpec)
+		if err != nil {
+			return errors.Wrap(err, "failed to create network")
 		}
 	}
 
-	// create network
-	networkStatus, err = s.createNetwork(ctx, &s.scope.HetznerCluster.Spec.NetworkSpec)
-	if err != nil {
-		return errors.Wrap(err, "failed to create network")
-	}
-	s.scope.HetznerCluster.Status.Network = networkStatus
+	s.scope.HetznerCluster.Status.Network = apiToStatus(network)
 	return nil
 }
 
-func (s *Service) createNetwork(ctx context.Context, spec *infrav1.NetworkSpec) (*infrav1.NetworkStatus, error) {
+func (s *Service) createNetwork(ctx context.Context, spec *infrav1.NetworkSpec) (*hcloud.Network, error) {
 	hc := s.scope.HetznerCluster
 
 	_, network, err := net.ParseCIDR(spec.CIDRBlock)
@@ -103,22 +58,22 @@ func (s *Service) createNetwork(ctx context.Context, spec *infrav1.NetworkSpec) 
 		return nil, errors.Wrapf(err, "invalid network '%s'", spec.CIDRBlock)
 	}
 
-	var subnets = make([]hcloud.NetworkSubnet, len(spec.Subnets))
-	for pos, sn := range spec.Subnets {
-		_, subnet, err := net.ParseCIDR(sn.CIDRBlock)
-		if err != nil {
-			return nil, errors.Wrapf(err, "invalid network '%s'", sn.CIDRBlock)
-		}
-		subnets[pos].IPRange = subnet
-		subnets[pos].NetworkZone = hcloud.NetworkZone(sn.NetworkZone)
-		subnets[pos].Type = hcloud.NetworkSubnetTypeServer
+	_, subnet, err := net.ParseCIDR(s.scope.HetznerCluster.Spec.NetworkSpec.CIDRBlock)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid network '%s'", s.scope.HetznerCluster.Spec.NetworkSpec.CIDRBlock)
 	}
 
 	opts := hcloud.NetworkCreateOpts{
 		Name:    hc.Name,
 		IPRange: network,
 		Labels:  s.labels(),
-		Subnets: subnets,
+		Subnets: []hcloud.NetworkSubnet{
+			{
+				IPRange:     subnet,
+				NetworkZone: hcloud.NetworkZone(s.scope.HetznerCluster.Spec.NetworkSpec.NetworkZone),
+				Type:        hcloud.NetworkSubnetTypeServer,
+			},
+		},
 	}
 
 	respNetworkCreate, _, err := s.scope.HCloudClient().CreateNetwork(ctx, opts)
@@ -137,54 +92,36 @@ func (s *Service) createNetwork(ctx context.Context, spec *infrav1.NetworkSpec) 
 		"Created network with opts %s",
 		opts)
 
-	return apiToStatus(respNetworkCreate), nil
+	return respNetworkCreate, nil
 }
 
-func (s *Service) deleteNetwork(ctx context.Context, status *infrav1.NetworkStatus) error {
-	// ensure deleted network is actually owned by us
-	clusterTagKey := infrav1.ClusterTagKey(s.scope.HetznerCluster.Name)
-	if status.Labels == nil || infrav1.ResourceLifecycle(status.Labels[clusterTagKey]) != infrav1.ResourceLifecycleOwned {
-		s.scope.V(2).Info("Ignore request to delete network, as it is not owned", "id", status.ID, "cidrBlock", status.CIDRBlock)
-		return nil
-	}
-	_, err := s.scope.HCloudClient().DeleteNetwork(ctx, &hcloud.Network{ID: status.ID})
+// Delete implements deletion of networks.
+func (s *Service) Delete(ctx context.Context) error {
+	_, err := s.scope.HCloudClient().DeleteNetwork(ctx, &hcloud.Network{ID: s.scope.HetznerCluster.Status.Network.ID})
 	if err != nil {
+		// If resource has been deleted already then do nothing
+		if hcloud.IsError(err, hcloud.ErrorCodeNotFound) {
+			s.scope.V(1).Info("deleting network failed - not found", "id", s.scope.HetznerCluster.Status.Network.ID)
+			return nil
+		}
 		record.Warnf(
 			s.scope.HetznerCluster,
 			"NetworkDeleteFailed",
-			"Failed to delete network with status %s",
-			status)
+			"Failed to delete network with ID %v",
+			s.scope.HetznerCluster.Status.Network.ID)
 		return err
 	}
 	record.Eventf(
 		s.scope.HetznerCluster,
 		"NetworkDeleted",
-		"Deleted network with status %s",
-		status)
-	s.scope.V(1).Info("Delete network", "id", status.ID, "cidrBlock", status.CIDRBlock)
-	return err
-}
-
-// Delete implements deletion of networks.
-func (s *Service) Delete(ctx context.Context) (err error) {
-	// update current status
-	networkStatus, err := s.actualStatus(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to refresh networks")
-	}
-
-	if networkStatus == nil {
-		return nil
-	}
-
-	if err := s.deleteNetwork(ctx, networkStatus); err != nil {
-		return errors.Wrap(err, "failed to delete network")
-	}
+		"Deleted network with ID %v",
+		s.scope.HetznerCluster.Status.Network.ID)
+	s.scope.V(1).Info("Delete network", "id", s.scope.HetznerCluster.Status.Network.ID)
 
 	return nil
 }
 
-func (s *Service) actualStatus(ctx context.Context) (*infrav1.NetworkStatus, error) {
+func (s *Service) findNetwork(ctx context.Context) (*hcloud.Network, error) {
 	opts := hcloud.NetworkListOpts{}
 	opts.LabelSelector = utils.LabelsToLabelSelector(s.labels())
 	networks, err := s.scope.HCloudClient().ListNetworks(ctx, opts)
@@ -192,9 +129,37 @@ func (s *Service) actualStatus(ctx context.Context) (*infrav1.NetworkStatus, err
 		return nil, err
 	}
 
-	for _, n := range networks {
-		return apiToStatus(n), nil
+	if len(networks) > 1 {
+		return nil, fmt.Errorf("found multiple networks with opts %v - not allowed", opts)
 	}
 
-	return nil, nil
+	if len(networks) == 0 {
+		return nil, nil
+	}
+
+	if len(networks[0].Subnets) > 1 {
+		return nil, fmt.Errorf("multiple subnets not allowed")
+	}
+
+	return networks[0], nil
+}
+
+func apiToStatus(network *hcloud.Network) *infrav1.NetworkStatus {
+	attachedServerIDs := make([]int, 0, len(network.Servers))
+	for _, s := range network.Servers {
+		attachedServerIDs = append(attachedServerIDs, s.ID)
+	}
+
+	return &infrav1.NetworkStatus{
+		ID:             network.ID,
+		Labels:         network.Labels,
+		AttachedServer: attachedServerIDs,
+	}
+}
+
+func (s *Service) labels() map[string]string {
+	clusterTagKey := infrav1.ClusterTagKey(s.scope.HetznerCluster.Name)
+	return map[string]string{
+		clusterTagKey: string(infrav1.ResourceLifecycleOwned),
+	}
 }
