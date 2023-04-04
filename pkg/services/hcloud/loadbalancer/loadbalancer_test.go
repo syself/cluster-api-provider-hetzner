fc5ee1320c12f3cd0969d8369417810926c32c19
janiskemper <janis.kemper@syself.com>
2023-04-04 15:10:33 +0200

    :bug: Fix wrong lb type
    Updating code of load balancer library to make it easier to read and
    understand. Fixing error that load balancer type was translated wrongly
    from the value in the spec of HetznerCluster to the load balancer create
    options. Adding further unit tests.

--- pkg/services/hcloud/loadbalancer/loadbalancer_test.go
+++ pkg/services/hcloud/loadbalancer/loadbalancer_test.go
@@ -17,18 +17,19 @@ limitations under the License.
 package loadbalancer
 
 import (
+	"github.com/go-logr/logr"
+	"github.com/hetznercloud/hcloud-go/hcloud"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	infrav1 "github.com/syself/cluster-api-provider-hetzner/api/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 var _ = Describe("Loadbalancer", func() {
 	Context("hcloud cluster has network attached", func() {
-		var sts infrav1.LoadBalancerStatus
+		var sts *infrav1.LoadBalancerStatus
 		BeforeEach(func() {
-			var err error
-			sts, err = apiToStatus(lb, true)
-			Expect(err).To(Succeed())
+			sts = statusFromHCloudLB(lb, true, logr.Discard())
 		})
 
 		It("should have two targets", func() {
@@ -46,11 +47,9 @@ var _ = Describe("Loadbalancer", func() {
 		})
 	})
 	Context("hcloud cluster has no network attached", func() {
-		var sts infrav1.LoadBalancerStatus
+		var sts *infrav1.LoadBalancerStatus
 		BeforeEach(func() {
-			var err error
-			sts, err = apiToStatus(lb, false)
-			Expect(err).To(Succeed())
+			sts = statusFromHCloudLB(lb, false, logr.Discard())
 		})
 
 		It("should have two targets", func() {
@@ -66,5 +65,82 @@ var _ = Describe("Loadbalancer", func() {
 		It("should be unprotected", func() {
 			Expect(sts.Protected).To(Equal(protected))
 		})
+	})
+})
+
+var _ = Describe("createOptsFromSpec", func() {
+	var hetznerCluster *infrav1.HetznerCluster
+	var wantCreateOpts hcloud.LoadBalancerCreateOpts
+	BeforeEach(func() {
+		lbName := "lb-name"
+		lbType := "lb11"
+		lbRegion := "fsn1"
+		controlPlaneEndpointPort := 22
+		lbPort := 6443
+		networkID := 42
+
+		hetznerCluster = &infrav1.HetznerCluster{
+			Spec: infrav1.HetznerClusterSpec{
+				ControlPlaneLoadBalancer: infrav1.LoadBalancerSpec{
+					Name:      &lbName,
+					Algorithm: infrav1.LoadBalancerAlgorithmTypeLeastConnections,
+					Type:      lbType,
+					Region:    infrav1.Region(lbRegion),
+					Port:      lbPort,
+				},
+				ControlPlaneEndpoint: &clusterv1.APIEndpoint{Port: int32(controlPlaneEndpointPort)},
+			},
+			Status: infrav1.HetznerClusterStatus{
+				Network: &infrav1.NetworkStatus{ID: networkID},
+			},
+		}
+		hetznerCluster.Name = "hetzner-cluster"
+
+		publicInterface := true
+		clusterTagKey := infrav1.ClusterTagKey(hetznerCluster.Name)
+		proxyprotocol := false
+
+		wantCreateOpts = hcloud.LoadBalancerCreateOpts{
+			LoadBalancerType: &hcloud.LoadBalancerType{Name: lbType},
+			Name:             lbName,
+			Algorithm:        &hcloud.LoadBalancerAlgorithm{Type: hcloud.LoadBalancerAlgorithmTypeLeastConnections},
+			Location:         &hcloud.Location{Name: lbRegion},
+			Network:          &hcloud.Network{ID: networkID},
+			Labels:           map[string]string{clusterTagKey: string(infrav1.ResourceLifecycleOwned)},
+			PublicInterface:  &publicInterface,
+			Services: []hcloud.LoadBalancerCreateOptsService{
+				{
+					Protocol:        hcloud.LoadBalancerServiceProtocolTCP,
+					ListenPort:      &controlPlaneEndpointPort,
+					DestinationPort: &lbPort,
+					Proxyprotocol:   &proxyprotocol,
+				},
+			},
+		}
+	})
+
+	It("creates specs for cluster without network", func() {
+		hetznerCluster.Status.Network = nil
+		wantCreateOpts.Network = nil
+
+		Expect(createOptsFromSpec(hetznerCluster)).To(Equal(wantCreateOpts))
+	})
+
+	It("creates specs for cluster with network", func() {
+		Expect(createOptsFromSpec(hetznerCluster)).To(Equal(wantCreateOpts))
+	})
+
+	It("creates specs for cluster without load balancer name set", func() {
+		hetznerCluster.Spec.ControlPlaneLoadBalancer.Name = nil
+
+		createOpts := createOptsFromSpec(hetznerCluster)
+
+		// should generate correct name
+		Expect(createOpts.Name).To(HavePrefix("hetzner-cluster-kube-apiserver-"))
+
+		// should be the same for all other specs
+		createOpts.Name = ""
+		wantCreateOpts.Name = ""
+		Expect(createOpts).To(Equal(wantCreateOpts))
 	})
 })
