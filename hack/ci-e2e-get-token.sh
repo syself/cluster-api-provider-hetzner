88b953b751c5df76c4f7c6cfaf15b72c1174193d
Anurag <81210977+kranurag7@users.noreply.github.com>
2023-12-03 18:50:09 +0530

    update script to update .envrc
    We used to use .envrc by hands and this commit updates the script to
    update the .envrc and load the environment variable automatically.
    
    Signed-off-by: Anurag <81210977+kranurag7@users.noreply.github.com>

--- hack/ci-e2e-get-token.sh
+++ hack/ci-e2e-get-token.sh
@@ -2,11 +2,12 @@
 
 set -eu
 
-curl \
-    --fail-with-body \
-    --retry 2 \
-    --silent \
-    -A "github.com/syself/cluster-api-provider-hetzner" \
-    --header "Authorization: Bearer $TPS_TOKEN" \
-    -X POST \
-    https://tps.hc-integrations.de/
+newtoken=$(curl --fail-with-body --retry 2 --silent -A "github.com/syself/cluster-api-provider-hetzner" --header 'Authorization: Bearer '"$TPS_TOKEN"'' -X POST https://tps.hc-integrations.de)
+
+if [ -e .envrc ]; then
+    echo "Updating .envrc"
+    sed -i "s/HCLOUD_TOKEN=.*/HCLOUD_TOKEN=$newtoken/" .envrc
+    if type -P direnv > /dev/null; then
+        direnv allow
+    fi
+fi
