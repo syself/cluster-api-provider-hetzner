33be5083e7ec4e63f39255485ce0c60c931baaee
Thomas Guettler <thomas.guettler@syself.com>
2025-03-18 09:47:37 +0100

    :seedling: Uprade builder image to Go 1.23

--- hack/upgrade-builder-image.sh
+++ hack/upgrade-builder-image.sh
@@ -16,28 +16,33 @@
 
 # This script is executed in the Update-Bot container.
 # It checks if the Dockerfile for the build container has changed.
-# If so, it uses the version of the main branch as the basis for creating a new image tag. 
-# The script also checks if the image tag for the build image exists in the main branch. 
-# We assume that only one branch and one PR will be created for the changes in the build container. 
+# If so, it uses the version of the main branch as the basis for creating a new image tag.
+# The script also checks if the image tag for the build image exists in the main branch.
+# We assume that only one branch and one PR will be created for the changes in the build container.
 # Therefore, we can update this branch as many times as we want and reuse the next available tag after the one from the main branch.
 
-set -o errexit
-set -o nounset
-set -o pipefail
+# Usage:
+# Step 1: Update images/builder/Dockerfile (for example increate Go version)
+# Step 2: Update .builder-image-version.txt
+# Step 3: Run this script
+
+# Bash Strict Mode: https://github.com/guettli/bash-strict-mode
+trap 'echo "Warning: A command has failed. Exiting the script. Line was ($0:$LINENO): $(sed -n "${LINENO}p" "$0")"; exit 3' ERR
 
 export BUILDER_IMAGE=ghcr.io/syself/caph-builder
 
-REPO_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
+REPO_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
 cd "${REPO_ROOT}" || exit 1
 
+# shellcheck disable=SC1091
 source "${REPO_ROOT}/hack/semver-upgrade.sh"
 
-if git diff --exit-code .builder-image-version.txt images/builder/Dockerfile images/builder/build.sh > /dev/null; then
+if git diff --exit-code .builder-image-version.txt images/builder/Dockerfile images/builder/build.sh >/dev/null; then
   echo "nothing seems to have changed."
   exit 0
 fi
 
-if [ "${CI:-false}" = true ] ; then
+if [ "${CI:-false}" = true ]; then
   echo "$BUILD_IMAGE_TOKEN" | docker login ghcr.io -u "$BUILD_IMAGE_USER" --password-stdin
 fi
 
@@ -51,19 +56,22 @@ export VERSION
 NEW_VERSION=$(semver_upgrade patch "$VERSION")
 export NEW_VERSION
 
-if ! docker manifest inspect "$BUILDER_IMAGE:$VERSION" > /dev/null; then
+if ! docker manifest inspect "$BUILDER_IMAGE:$VERSION" >/dev/null; then
   echo "could not find image $BUILDER_IMAGE:$VERSION"
   exit 1
 fi
-echo "$NEW_VERSION" > .builder-image-version.txt
+echo "$NEW_VERSION" >.builder-image-version.txt
 echo "Wrote new version $NEW_VERSION to .builder-image-version.txt"
 
-if docker manifest inspect ghcr.io/syself/caph-builder:${NEW_VERSION} > /dev/null ; echo $?; then
-  
+if
+  docker manifest inspect ghcr.io/syself/caph-builder:"${NEW_VERSION}" >/dev/null
+  echo $?
+then
+
   sed -i -e "/^BUILDER_IMAGE_VERSION /s/:=.*$/:= ${NEW_VERSION}/" Makefile
   grep -r -E 'ghcr.io/syself/caph-builder:[0-9].*.*' -l | xargs sed -i -e "s/ghcr.io\/syself\/caph-builder:${VERSION}/ghcr.io\/syself\/caph-builder:${NEW_VERSION}/g"
-  docker build -t ghcr.io/syself/caph-builder:${NEW_VERSION}  ./images/builder
-  docker push ghcr.io/syself/caph-builder:${NEW_VERSION}
+  docker build -t ghcr.io/syself/caph-builder:"${NEW_VERSION}" ./images/builder
+  docker push ghcr.io/syself/caph-builder:"${NEW_VERSION}"
 else
   exit 1
 fi
