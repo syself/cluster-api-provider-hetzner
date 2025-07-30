5391665a258ca77a6787eec2ace6ce02dfea3b01
Thomas Guettler <thomas.guettler@syself.com>
2025-07-30 10:34:17 +0200

    :seedling: Add SkipCreatingHetznerSecretInWorkloadCluster to hetznercluster_controller
    SkipCreatingHetznerSecretInWorkloadCluster indicates whether the Hetzner secret should be created in the workload cluster. By default the secret gets created, so that the ccm (running in the wl-cluster) can use that secret. If you prefer to not reveal the secret in the workload cluster, you can set this to value to false, so that the secret is not created. Be sure to run the ccm outside of the workload cluster in that case, e.g. in the management cluster.
    
    Closes #1636

--- api/v1beta1/hetznercluster_types.go
+++ api/v1beta1/hetznercluster_types.go
@@ -64,6 +64,10 @@ type HetznerClusterSpec struct {
 	// HetznerSecretRef is a reference to a token to be used when reconciling this cluster.
 	// This is generated in the security section under API TOKENS. Read & write is necessary.
 	HetznerSecret HetznerSecretRef `json:"hetznerSecretRef"`
+
+	// SkipCreatingHetznerSecretInWorkloadCluster indicates whether the Hetzner secret should be created in the workload cluster. By default the secret gets created, so that the ccm (running in the wl-cluster) can use that secret. If you prefer to not reveal the secret in the workload cluster, you can set this to value to false, so that the secret is not created. Be sure to run the ccm outside of the workload cluster in that case, e.g. in the management cluster.
+	// +optional
+	SkipCreatingHetznerSecretInWorkloadCluster bool `json:"skipCreatingHetznerSecretInWorkloadCluster,omitempty"`
 }
 
 // HetznerClusterStatus defines the observed state of HetznerCluster.
