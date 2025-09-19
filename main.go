edcaf4efcca58204286e0f1d53c9f52eec74816b
Thomas Guettler <thomas.guettler@syself.com>
2025-09-19 10:15:41 +0200

    :seedling: Provision baremetal via --baremetal-image-url-command

--- main.go
+++ main.go
@@ -85,7 +85,8 @@ var (
 	syncPeriod                         time.Duration
 	rateLimitWaitTime                  time.Duration
 	preProvisionCommand                string
-	imageURLCommand                    string
+	hcloudImageURLCommand              string
+	baremetalImageURLCommand           string
 	skipWebhooks                       bool
 )
 
@@ -107,7 +108,8 @@ func main() {
 	fs.DurationVar(&rateLimitWaitTime, "rate-limit", 5*time.Minute, "The rate limiting for HCloud controller (e.g. 5m)")
 	fs.BoolVar(&hcloudclient.DebugAPICalls, "debug-hcloud-api-calls", false, "Debug all calls to the hcloud API.")
 	fs.StringVar(&preProvisionCommand, "pre-provision-command", "", "Command to run (in rescue-system) before installing the image on bare metal servers. You can use that to check if the machine is healthy before installing the image. If the exit value is non-zero, the machine is considered unhealthy. This command must be accessible by the controller pod. You can use an initContainer to copy the command to a shared emptyDir.")
-	fs.StringVar(&imageURLCommand, "hcloud-image-url-command", "", "Command to run (in rescue-system) to provision an hcloud machine. The command will get the imageURL, bootstrap-data and machine-name of the corresponding hcloudmachine as argument. It is up to the command to download from that URL and provision the disk accordingly. This command must be accessible by the controller pod. You can use an initContainer to copy the command to a shared emptyDir. The env var OCI_REGISTRY_AUTH_TOKEN from the caph process will be set for the command, too. The command must end with the last line containing IMAGE_URL_DONE. Otherwise the execution is considered to have failed. Related https://pkg.go.dev/github.com/syself/cluster-api-provider-hetzner/api/v1beta1#HCloudMachineSpec.ImageURL")
+	fs.StringVar(&hcloudImageURLCommand, "hcloud-image-url-command", "", "Command to run (in rescue-system) to provision an hcloud machine. The command will get the imageURL, bootstrap-data and machine-name of the corresponding hcloudmachine as argument. It is up to the command to download from that URL and provision the disk accordingly. This command must be accessible by the controller pod. You can use an initContainer to copy the command to a shared emptyDir. The env var OCI_REGISTRY_AUTH_TOKEN from the caph process will be set for the command, too. The command must end with the last line containing IMAGE_URL_DONE. Otherwise the execution is considered to have failed. Related https://pkg.go.dev/github.com/syself/cluster-api-provider-hetzner/api/v1beta1#HCloudMachineSpec.ImageURL")
+	fs.StringVar(&baremetalImageURLCommand, "baremetal-image-url-command", "", "Command to run (in rescue-system) to provision an baremetal machine. The command will get the imageURL, bootstrap-data and machine-name of the corresponding hetznerbaremtelmachine as argument. It is up to the command to download from that URL and provision the disk accordingly. This command must be accessible by the controller pod. You can use an initContainer to copy the command to a shared emptyDir. The env var OCI_REGISTRY_AUTH_TOKEN from the caph process will be set for the command, too. The command must end with the last line containing IMAGE_URL_DONE. Otherwise the execution is considered to have failed. Related https://pkg.go.dev/github.com/syself/cluster-api-provider-hetzner/api/v1beta1#HetznerBareMealMachineSpec.ImageURL")
 	fs.BoolVar(&skipWebhooks, "skip-webhooks", false, "Skip setting up of webhooks. Together with --leader-elect=false, you can use `go run main.go` to run CAPH in a cluster connected via KUBECONFIG. You should scale down the caph deployment to 0 before doing that. This is only for testing!")
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
@@ -131,15 +133,15 @@ func main() {
 	}
 
 	// If ImageURLCommand is set, check if the file exists and validate the basename.
-	if imageURLCommand != "" {
-		baseName := filepath.Base(imageURLCommand)
+	if hcloudImageURLCommand != "" {
+		baseName := filepath.Base(hcloudImageURLCommand)
 		if !commandRegex.MatchString(baseName) {
 			msg := fmt.Sprintf("basename (%s) must match the regex %s", baseName, commandRegex.String())
 			setupLog.Error(errors.New(msg), "")
 			os.Exit(1)
 		}
 
-		_, err := os.Stat(imageURLCommand)
+		_, err := os.Stat(hcloudImageURLCommand)
 		if err != nil {
 			setupLog.Error(err, "hcloud-image-url-command not found")
 			os.Exit(1)
@@ -212,7 +214,7 @@ func main() {
 		HCloudClientFactory: hcloudClientFactory,
 		SSHClientFactory:    sshclient.NewFactory(),
 		WatchFilterValue:    watchFilterValue,
-		ImageURLCommand:     imageURLCommand,
+		ImageURLCommand:     hcloudImageURLCommand,
 	}).SetupWithManager(ctx, mgr, controller.Options{MaxConcurrentReconciles: hcloudMachineConcurrency}); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "HCloudMachine")
 		os.Exit(1)
@@ -237,6 +239,7 @@ func main() {
 		RateLimitWaitTime:   rateLimitWaitTime,
 		WatchFilterValue:    watchFilterValue,
 		PreProvisionCommand: preProvisionCommand,
+		ImageURLCommand:     baremetalImageURLCommand,
 	}).SetupWithManager(ctx, mgr, controller.Options{MaxConcurrentReconciles: hetznerBareMetalHostConcurrency}); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "HetznerBareMetalHost")
 		os.Exit(1)
