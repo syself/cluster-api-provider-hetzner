2ff488f1553846878cb06eb9a07952eeb6efd982
Thomas Guettler <thomas.guettler@syself.com>
2025-12-06 11:37:02 +0100

    From:
    Files with worst coverage:
     MISSING  FILE
         441  github.com/syself/cluster-api-provider-hetzner/pkg/services/baremetal/host/host.go
         259  github.com/syself/cluster-api-provider-hetzner/pkg/services/hcloud/server/server.go
         226  github.com/syself/cluster-api-provider-hetzner/pkg/services/baremetal/baremetal/baremetal.go
         226  github.com/syself/cluster-api-provider-hetzner/pkg/services/baremetal/client/ssh/ssh_client.go
         186  github.com/syself/cluster-api-provider-hetzner/pkg/services/hcloud/loadbalancer/loadbalancer.go
    
    to
    
    Files with worst coverage:
     MISSING  FILE
         350  github.com/syself/cluster-api-provider-hetzner/pkg/services/baremetal/host/host.go
         259  github.com/syself/cluster-api-provider-hetzner/pkg/services/hcloud/server/server.go
         226  github.com/syself/cluster-api-provider-hetzner/pkg/services/baremetal/baremetal/baremetal.go
         226  github.com/syself/cluster-api-provider-hetzner/pkg/services/baremetal/client/ssh/ssh_client.go
         186  github.com/syself/cluster-api-provider-hetzner/pkg/services/hcloud/loadbalancer/loadbalancer.go

--- hack/test-unit.sh
+++ hack/test-unit.sh
@@ -60,3 +60,5 @@ go run github.com/gametimesf/gocov/gocov@latest convert .coverage/cover.out |
     go run github.com/AlekSi/gocov-xml@latest >.coverage/cover.xml
 
 pipx run pycobertura show --format html --output .coverage/cover.html --source "$PWD" .coverage/cover.xml
+
+echo "Created .coverage/cover.html"
