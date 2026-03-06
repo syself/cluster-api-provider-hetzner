f2e3a1ae9d16bd922578d8a04fd1a3c210ba1675
Thomas Güttler <thomas.guettler@syself.com>
2026-03-06 09:42:36 +0100

    e2e: sync env/ssh workflow handling with tg/get-e2e-tests-working

--- hack/ensure-env-variables.sh
+++ hack/ensure-env-variables.sh
@@ -32,15 +32,23 @@ if [ ${#missing_vars[@]} -gt 0 ]; then
 fi
 
 if [[ -n "${HETZNER_SSH_PUB:-}" ]]; then
-  if ! printf "%s" "$HETZNER_SSH_PUB" | base64 -d >/dev/null 2>&1; then
-    echo "env var HETZNER_SSH_PUB seems wrong. It should be base64 encoded. Guess: update .envrc"
+  if ! decoded_pub="$(printf '%s' "$HETZNER_SSH_PUB" | base64 --decode 2>/dev/null)"; then
+    echo "env var HETZNER_SSH_PUB seems wrong. It should be the base64-encoded pub key. Guess: update .envrc"
+    exit 1
+  fi
+  if ! printf '%s' "$decoded_pub" | grep -q -- 'ssh-'; then
+    echo "env var HETZNER_SSH_PUB seems wrong. It should be the base64-encoded pub key. Guess: update .envrc"
     exit 1
   fi
 fi
 
 if [[ -n "${HETZNER_SSH_PRIV:-}" ]]; then
-  if ! printf "%s" "$HETZNER_SSH_PRIV" | base64 -d >/dev/null 2>&1; then
-    echo "env var HETZNER_SSH_PRIV seems wrong. It should be base64 encoded. Guess: update .envrc"
+  if ! decoded_priv="$(printf '%s' "$HETZNER_SSH_PRIV" | base64 --decode 2>/dev/null)"; then
+    echo "env var HETZNER_SSH_PRIV seems wrong. It should be the base64-encoded priv key. Guess: update .envrc"
+    exit 1
+  fi
+  if ! printf '%s' "$decoded_priv" | grep -q -- 'OPENSSH'; then
+    echo "env var HETZNER_SSH_PRIV seems wrong. It should be the base64-encoded priv key. Guess: update .envrc"
     exit 1
   fi
 fi
@@ -54,8 +62,21 @@ if [[ -n "${KUBERNETES_VERSION:-}" ]]; then
     exit 1
   fi
 
-  if [[ "$(printf "%s\n%s\n" "$min_kubernetes_version" "$kubernetes_version" | sort -V | head -n 1)" != "$min_kubernetes_version" ]]; then
+  if [[ "$(printf '%s\n%s\n' "$min_kubernetes_version" "$kubernetes_version" | sort -V | head -n 1)" != "$min_kubernetes_version" ]]; then
     echo "KUBERNETES_VERSION must be >= v${min_kubernetes_version}, got '$KUBERNETES_VERSION'."
     exit 1
   fi
 fi
+
+for varname in "$@"; do
+  if [ "$varname" = "HCLOUD_CONTROL_PLANE_MACHINE_TYPE" ] || [ "$varname" = "HCLOUD_WORKER_MACHINE_TYPE" ]; then
+    deprecated_types=(cx22 cx32 cx42 cx52 cpx11 cpx21 cpx31 cpx41 cpx51)
+    for deprecated in "${deprecated_types[@]}"; do
+      if [[ "${!varname}" == *"$deprecated"* ]]; then
+        echo "$varname contains deprecated type '$deprecated'."
+        echo "Deprecated types: ${deprecated_types[*]}"
+        exit 1
+      fi
+    done
+  fi
+done
