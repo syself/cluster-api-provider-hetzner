4bceeac15aef683fb948432435cf6f02e96e5773
Thomas Güttler <thomas.guettler@syself.com>
2026-03-09 12:07:38 +0100

    implement feedback, keep log.

--- pkg/services/baremetal/host/host.go
+++ pkg/services/baremetal/host/host.go
@@ -1759,6 +1759,7 @@ func (s *Service) actionEnsureProvisioned(ctx context.Context) (ar actionResult)
 	if hostname != wantHostName {
 		// give the reboot some time until it takes effect
 		if s.hasJustRebooted() {
+			s.scope.Info("ensureProvisioned: hasJustRebooted. Retrying...", "hostname", hostname)
 			markProvisionPendingWithInfo(s.scope.HetznerBareMetalHost,
 				infrav1.StateEnsureProvisioned, "host has just rebooted")
 			return actionContinue{delay: 2 * time.Second}
