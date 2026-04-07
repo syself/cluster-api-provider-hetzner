bd3b20acfdd31e07bc408679a2d9f37ce2639d1a
Thomas Güttler <thomas.guettler@syself.com>
2026-04-07 12:48:18 +0200

    make PR easier to understand.

--- hack/boilerplate/testdata/default/fail.sh
+++ hack/boilerplate/testdata/default/fail.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2022 Kubernetes Authors.
+# Copyright 2022 Invalid Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## invalid header
+## invalid header on purpose
 
 exit_code=42
 exit $exit_code
