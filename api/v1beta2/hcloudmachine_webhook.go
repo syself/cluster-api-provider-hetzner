c6e29a42db806b3d64742bbe0539e2df76012be0
Abdullah Shakeel <abdullah.shakeel@syself.com>
2026-05-18 18:29:13 +0500

    🌱 Copy api/v1beta1/ to api/v1beta2/ with mechanical reference renames

---
+++ api/v1beta2/hcloudmachine_webhook.go
@@ -0,0 +1,88 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta2
+
+import (
+	"context"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/syself/cluster-api-provider-hetzner/pkg/utils"
+)
+
+type hcloudMachineWebhook struct{}
+
+// log is for logging in this package.
+var hcloudmachinelog = utils.GetDefaultLogger("info").WithName("hcloudmachine-resource")
+
+// SetupWebhookWithManager initializes webhook manager for HCloudMachine.
+func (r *HCloudMachine) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	w := new(hcloudMachineWebhook)
+	return ctrl.NewWebhookManagedBy(mgr, r).
+		WithDefaulter(w).
+		WithValidator(w).
+		Complete()
+}
+
+// SetupWebhookWithManager initializes webhook manager for HCloudMachineList.
+func (r *HCloudMachineList) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr, r).
+		Complete()
+}
+
+//+kubebuilder:webhook:path=/mutate-infrastructure-cluster-x-k8s-io-v1beta1-hcloudmachine,mutating=true,failurePolicy=fail,sideEffects=None,groups=infrastructure.cluster.x-k8s.io,resources=hcloudmachines,verbs=create;update,versions=v1beta1,name=mutation.hcloudmachine.infrastructure.cluster.x-k8s.io,admissionReviewVersions={v1,v1beta1}
+
+var _ admission.Defaulter[*HCloudMachine] = &hcloudMachineWebhook{}
+
+// Default implements admission.Defaulter[*HCloudMachine] so a webhook will be registered for the type.
+func (*hcloudMachineWebhook) Default(_ context.Context, r *HCloudMachine) error {
+	if r.Spec.PublicNetwork == nil {
+		r.Spec.PublicNetwork = &PublicNetworkSpec{
+			EnableIPv4: true,
+			EnableIPv6: true,
+		}
+	}
+	return nil
+}
+
+//+kubebuilder:webhook:path=/validate-infrastructure-cluster-x-k8s-io-v1beta1-hcloudmachine,mutating=false,failurePolicy=fail,sideEffects=None,groups=infrastructure.cluster.x-k8s.io,resources=hcloudmachines,verbs=create;update,versions=v1beta1,name=validation.hcloudmachine.infrastructure.cluster.x-k8s.io,admissionReviewVersions={v1,v1beta1}
+
+var _ admission.Validator[*HCloudMachine] = &hcloudMachineWebhook{}
+
+// ValidateCreate implements admission.Validator[*HCloudMachine] so a webhook will be registered for the type.
+func (*hcloudMachineWebhook) ValidateCreate(_ context.Context, r *HCloudMachine) (admission.Warnings, error) {
+	hcloudmachinelog.V(1).Info("validate create", "name", r.Name)
+
+	allErrs := validateHCloudMachineSpec(r.Spec)
+
+	return nil, aggregateObjErrors(r.GroupVersionKind().GroupKind(), r.Name, allErrs)
+}
+
+// ValidateUpdate implements admission.Validator[*HCloudMachine] so a webhook will be registered for the type.
+func (*hcloudMachineWebhook) ValidateUpdate(_ context.Context, oldM, r *HCloudMachine) (admission.Warnings, error) {
+	hcloudmachinelog.V(1).Info("validate update", "name", r.Name)
+
+	allErrs := validateHCloudMachineSpecUpdate(oldM.Spec, r.Spec)
+
+	return nil, aggregateObjErrors(r.GroupVersionKind().GroupKind(), r.Name, allErrs)
+}
+
+// ValidateDelete implements admission.Validator[*HCloudMachine] so a webhook will be registered for the type.
+func (*hcloudMachineWebhook) ValidateDelete(context.Context, *HCloudMachine) (admission.Warnings, error) {
+	return nil, nil
+}
---
+++ api/v1beta2/hcloudremediationtemplate_webhook.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta2
+
+import (
+	"context"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+type hcloudRemediationTemplateWebhook struct{}
+
+// SetupWebhookWithManager initializes webhook manager for HCloudRemediationTemplate.
+func (r *HCloudRemediationTemplate) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	w := new(hcloudRemediationTemplateWebhook)
+	return ctrl.NewWebhookManagedBy(mgr, r).
+		WithValidator(w).
+		WithDefaulter(w).
+		Complete()
+}
+
+//+kubebuilder:webhook:path=/mutate-infrastructure-cluster-x-k8s-io-v1beta1-hcloudremediationtemplate,mutating=true,failurePolicy=fail,sideEffects=None,groups=infrastructure.cluster.x-k8s.io,resources=hcloudremediationtemplates,verbs=create;update,versions=v1beta1,name=mhcloudremediationtemplate.kb.io,admissionReviewVersions=v1
+
+var _ admission.Defaulter[*HCloudRemediationTemplate] = &hcloudRemediationTemplateWebhook{}
+
+// Default implements admission.Defaulter[*HCloudRemediationTemplate] so a webhook will be registered for the type.
+func (*hcloudRemediationTemplateWebhook) Default(context.Context, *HCloudRemediationTemplate) error {
+	return nil
+}
+
+//+kubebuilder:webhook:path=/validate-infrastructure-cluster-x-k8s-io-v1beta1-hcloudremediationtemplate,mutating=false,failurePolicy=fail,sideEffects=None,groups=infrastructure.cluster.x-k8s.io,resources=hcloudremediationtemplates,verbs=create;update,versions=v1beta1,name=vhcloudremediationtemplate.kb.io,admissionReviewVersions=v1
+
+var _ admission.Validator[*HCloudRemediationTemplate] = &hcloudRemediationTemplateWebhook{}
+
+// ValidateCreate implements admission.Validator[*HCloudRemediationTemplate] so a webhook will be registered for the type.
+func (*hcloudRemediationTemplateWebhook) ValidateCreate(context.Context, *HCloudRemediationTemplate) (admission.Warnings, error) {
+	return nil, nil
+}
+
+// ValidateUpdate implements admission.Validator[*HCloudRemediationTemplate] so a webhook will be registered for the type.
+func (*hcloudRemediationTemplateWebhook) ValidateUpdate(_ context.Context, _, _ *HCloudRemediationTemplate) (admission.Warnings, error) {
+	return nil, nil
+}
+
+// ValidateDelete implements admission.Validator[*HCloudRemediationTemplate] so a webhook will be registered for the type.
+func (*hcloudRemediationTemplateWebhook) ValidateDelete(context.Context, *HCloudRemediationTemplate) (admission.Warnings, error) {
+	return nil, nil
+}
