abf14b865ed93cf54131c29b8a531b2e1ee7e670
Dhairya Arora <dhairya.arora@syself.com>
2026-09-10 16:54:19 +0530

    fixup! switch to mgr.GetEventRecorderFor to get the event recorder

--- controllers/hetznerbaremetalmachine_controller_test.go
+++ controllers/hetznerbaremetalmachine_controller_test.go
@@ -609,7 +609,7 @@ var _ = Describe("HetznerBareMetalMachineReconciler", func() {
 
 				By("waiting for the successful kubeadm reset event")
 				Eventually(func() bool {
-					return hasEvent(ctx, testEnv, testNs.Name, host.Name, "SuccessfulResetKubeAdm", "Reset was successful.")
+					return hasEvent(ctx, testEnv, testNs.Name, host.Name, "SuccessfulResetKubeadm", "Reset was successful.")
 				}, timeout, time.Second).Should(BeTrue())
 			})
 
@@ -1393,7 +1393,8 @@ func Test_BareMetalHostToBareMetalMachines(t *testing.T) {
 		},
 	}
 	c := fakeclient.NewClientBuilder().WithScheme(scheme).WithObjects(
-		host, hbmm, hbmmOtherNS, hbmmWithoutLabel, hbmmWithHostAnnotation).Build()
+		host, hbmm, hbmmOtherNS, hbmmWithoutLabel, hbmmWithHostAnnotation,
+	).Build()
 
 	// host does not have a label.
 	f := BareMetalHostToBareMetalMachines(c, logr.Logger{})
